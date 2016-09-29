c3fe793d5e92ddb0519c6cef81beb73df98bfe44
Michael Raimi <mar2260@columbia.edu>
2016-09-28 22:18:04 -0400

    Fill table with redis data

--- app/views.py
+++ app/views.py
@@ -1,15 +1,29 @@
 from app import app
-from flask import redirect
+from flask import Flask, redirect, jsonify, render_template, request
 import redis
 
-@app.route('/')
-@app.route('/index')
-def get_messages():
-    ps = redis.StrictRedis(host='ec2-52-54-82-137.compute-1.amazonaws.com', port=6379, db=0, password='').pubsub()
-    ps.subscribe('tcp.private')
-    for message in ps.listen():
-        print str(message['data'])
-
 @app.route('/deck')
-def deck():  
+def deck():
   return redirect("https://docs.google.com/presentation/d/10Wux1jnxM16b4E1cBmffjlPYOxQiJFZVGKsUBoEwRWE/edit?usp=sharing", code=302)
+
+@app.route('/_pull')
+def get_messages():
+    print "get_messages called"
+    ps = redis.StrictRedis(host='ec2-52-54-82-137.compute-1.amazonaws.com', port=6379, db=0, password='BFHW6zDv3g7kuxDxRXV7K8Y2pdyfR7kw').pubsub()
+    ps.subscribe('tcp.private')
+    payloads = []
+    for i in xrange(0,5):
+        msg = ps.get_message(True, timeout=1000)
+        if msg:
+            print str(msg['data'])
+            vals = str(msg['data']).split(",")
+            payloads.append({"Connection": vals[0], "Protocol": vals[1], "Classification": vals[2]})
+    return jsonify(payloads=payloads)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
